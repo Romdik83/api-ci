872e8753262b7437a817cf5caee54a5ac8c8eb91
Romdik <romdik83@gmail.com>
2025-07-24 13:01:47 +0300

    mobole bank

---
+++ src/test/java/MobileBankApiTestV1.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiTestV1 {
+
+    @Test
+
+    void shouldReturnAccounts() {
+
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1/")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .header("content-type", "application/json; charset=UTF-8")
+                .body("", hasSize(3))
+                .body("[0].id", equalTo(1))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[1].balance", greaterThan(0))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+
+}
